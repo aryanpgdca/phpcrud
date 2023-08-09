ce1f3110377fbfbe10d10f45f98da8db3b8c9d57
Aryan Porwal <118986780+aryanpgdca@users.noreply.github.com>
2023-08-09 17:49:50 +0530

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -1,5 +1,5 @@
 <?php
-$con=mysqli_connect("localhost", "root", "", "phpcrud");
+$con=mysqli_connect("sql100.infinityfree.com", "if0_34783495", "96341270", "if0_34783495_phpcrud");
 if(mysqli_connect_errno())
 {
 echo "Connection Fail".mysqli_connect_error();
